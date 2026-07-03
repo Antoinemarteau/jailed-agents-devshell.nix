c7231145cc7b8f3165e6c578dadb89928ea6872d
Antoine Marteau <antoine.marteau@protonmail.com>
2026-07-03 11:25:49 +1000

    add default startup.jl

--- devshell_tmux_with_jailed_agents/devshell-home.nix
+++ devshell_tmux_with_jailed_agents/devshell-home.nix
@@ -7,6 +7,26 @@ home-manager.lib.homeManagerConfiguration {
     home.homeDirectory = devshellRoot + "/" + devshellHomeFolder;
     home.stateVersion = "25.11";
 
+    home.file.".julia/config/startup.jl".text = ''
+      import Pkg as var"#Pkg"
+
+      if isnothing(Base.find_package("Revise"))
+          var"#Pkg".add("Revise")
+      end
+
+      if !isfile(joinpath(first(DEPOT_PATH), "bin", "kaimon"))
+          var"#Pkg".Apps.add("Kaimon")
+      end
+
+      atreplinit() do repl
+          try
+              @eval using Revise
+          catch e
+              @warn "Could not load Revise" exception = e
+          end
+      end
+    '';
+
     programs = {
       zsh = {
         enable = true;
