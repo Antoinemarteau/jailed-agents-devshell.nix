bbf069835d71253f0b43ce2827aa9496f817407d
Antoine Marteau <antoine.marteau@protonmail.com>
2026-07-16 10:40:36 +1000

    personalize startup.jl

--- agentshome/.julia/config/startup.jl
+++ agentshome/.julia/config/startup.jl
@@ -1,21 +1,80 @@
 ENV["PYTHON"] = Sys.which("python") # For PyCall.jl / Plots
 
+if isinteractive()
+    ENV["JULIA_EDITOR"] = "nvim"
+
+    import Pkg as var"#Pkg"
+    let
+        pkgs = [
+            "BasicAutoloads", "Revise", "OhMyREPL",
+            "BenchmarkTools", "Chairmarks", "Cthulhu", "Debugger",
+            "Profile", "ProfileView", "Test", "StaticArrays",
+            "LinearAlgebra", "About", "Pluto", "FillArrays",
+            "Kaimon", "KaimonGate", "TestEnv"
+        ]
+        for pkg in pkgs
+            if Base.find_package(pkg) === nothing
+                var"#Pkg".add(pkg)
+            end
+        end
+
+        # JETLS.jl
+        #if isnothing(var"#Pkg".App.status( var"#Pkg".PackageSpec("JETLS")))
+        ## This either installs or updates JETLS
+        #redirect_stderr(devnull) do # makes it silent
+        #  var"#Pkg".Apps.add(; url="https://github.com/aviatesk/JETLS.jl", rev="2026-02-27")
+        #end
+        #end
+
+        # LanguageServer.jl
+        # it should be installed in special folder for neovim
+        mkpath("/home/antoine/.julia/environments/nvim-lspconfig")
+        var"#Pkg".activate("/home/antoine/.julia/environments/nvim-lspconfig")
+        if isnothing(Base.find_package("LanguageServer"))
+            var"#Pkg".add("LanguageServer")
+        end
+        if isnothing(Base.find_package("SymbolServer"))
+            var"#Pkg".add("SymbolServer")
+        end
+        if isnothing(Base.find_package("StaticLint"))
+            var"#Pkg".add("StaticLint")
+        end
+        var"#Pkg".activate()
+    end
+
+    using Revise
+
+    if isfile("Project.toml") #&& isfile("Manifest.toml")
+        var"#Pkg".activate(".")
+    end
+
+    import BasicAutoloads
+    BasicAutoloads.register_autoloads([
+        ["@btime", "@benchmark"] => :(using BenchmarkTools),
+        ["@b", "@be"]            => :(using Chairmarks),
+        ["@test", "@testset", "@test_broken", "@test_deprecated", "@test_logs",
+        "@test_nowarn", "@test_skip", "@test_throws", "@test_warn", "@inferred"]
+                                 => :(using Test),
+        ["@descend", "@descend_code_typed", "@descend_code_warntype"] =>
+                                    :(using Cthulhu),
+        ["@enter", "@run"]       => :(using Debugger),
+        ["@profile"]             => :(using Profile),
+        ["@profview", "@profview_allocs"]
+                                 => :(using ProfileView),
+        ["norm", "I"]            => :(using LinearAlgebra),
+        ["about"]                => :(using About),
+        ["MVector", "SVector", "MMatrix", "SMatrix", "MArray", "SArray"] =>
+                                    :(using StaticArrays),
+
+       #["pager"]                => :(using TerminalPager),
+       #["cowsay"]               => :(cowsay(x) = println("Cow: \"$x\"")),
+    ])
+end
+
 import Pkg as var"#Pkg"
-
-if isnothing(Base.find_package("Revise"))
-    var"#Pkg".add("Revise")
-end
-
-if isnothing(Base.find_package("TestEnv"))
-    var"#Pkg".add("TestEnv")
-end
 
 if !isfile(joinpath(first(DEPOT_PATH), "bin", "kaimon"))
     var"#Pkg".Apps.add("Kaimon")
-end
-
-if isnothing(Base.find_package("KaimonGate"))
-    var"#Pkg".add("KaimonGate")
 end
 
 try
